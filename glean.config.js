40ca6f62e9cd02933c65c7770b5b21b604171172
Malek Azadegan <azadeganmalek@gmail.com>
2025-06-25 17:02:49 -0400

    refactor: clean up glean config and update social media links

--- glean.config.js
+++ glean.config.js
@@ -6,33 +6,5 @@ module.exports = {
     "!lib/PHPMailer"
   ],
   "blocks": {
-    "nextprev": [
-      "views/partials/content_navbar.php",
-      "views/partials/dashboard_navbar.php",
-      "views/partials/footer.php",
-      "views/partials/head.php",
-      "views/partials/lesson_modal.php",
-      "views/partials/section_modal.php",
-      "views/partials/toast.php",
-      "views/partials/unit_modal.php",
-      "views/show_lesson.php",
-      "views/show_section.php",
-      "views/show_unit.php",
-      "views/units.php",
-      "public/index.php",
-      "models/Lesson.php",
-      "models/Section.php",
-      "models/Unit.php",
-      "includes/init.php",
-      "controllers/AudioController.php",
-      "controllers/AuthController.php",
-      "controllers/ContentsController.php",
-      "controllers/DashboardController.php",
-      "controllers/LessonController.php",
-      "controllers/PageController.php",
-      "controllers/SectionController.php",
-      "controllers/UnitController.php",
-      "controllers/UserController.php"
-    ]
   }
 };
